ed0937ed45a86a4b7a2c8a77db36b0c708e25884
Miguel Cuadros <cuadros_miguel@hotmail.com>
2022-09-20 15:07:05 -0300

    Correciones Preentrega1Cuadros

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-import CartWidget from './components/CartWidget';
-import List from './components/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
@@ -12,7 +10,7 @@ function App() {
         <NavBar></NavBar>
       </header>
       <main>
-        <List/>
+        <ItemListContainer greeting={'Contenido de greeting pasado a traves de ItemListContainer!!'}/>
       </main> 
     </div>
   );
