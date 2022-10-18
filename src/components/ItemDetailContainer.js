b854f720202ef97a0f5147e1ee6a854c555b3384
Miguel Cuadros <cuadros_miguel@hotmail.com>
2022-10-18 16:26:44 -0300

    Firebase2

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -7,40 +7,13 @@ import Item from "./Item";
 // import {doc, getDoc, getFirestore} from 'firebase/firestore';
 import {collection, getDocs, getFirestore, query, where, getDoc, doc} from 'firebase/firestore';
 
-// const db = getFirestore()
 
-// const colRef = collection(db , "items")
-// const qSnap = await getDocs(query(colRef, where("category", "==", "novillo")));
-
-// if (qSnap.size) {
-//   const data = qSnap.docs[0].data();
-// } else {
-//   console.log("No se encontro la categoria")
-// }
 
 function ItemDetailContainer(){
   const [item,setItem]=useState({})
   const {id} = useParams()
   const [category, setCategory] = useState({})
 
-  // useEffect(()=> {
-  //   const db = getFirestore()
-  //   const data = doc(db, 'items', 'NGUsqa0smwJQC7Wv52xo')
-  //   getDoc(data)
-  //     .then((value)=>{
-  //       console.log(value.data());
-  //     })  }, [])
-
-  // useEffect(()=> {
-  //   const db = getFirestore()
-  //   const data = collection(db, 'items')
-  //   getDocs(data)
-  //     .then((value)=>{
-  //       setCategory(value.docs.map((value)=>{
-  //         return value.data() 
-  //       }));
-  //     });  
-  //   }, [])
 
   useEffect(()=> {
     const db = getFirestore()
@@ -54,39 +27,6 @@ function ItemDetailContainer(){
   }, [])
       
 
-  // useEffect(()=> {
-  //   const db = getFirestore()
-  //   const data = query(collection(db, 'items'), where("category", "==", "novillo"))
-  //   // Ejemplo de aplicacion a proyecto
-  //   // const documento = doc (db, "items", prop.name) 
-  //   getDocs(data)
-  //     .then((value)=>{
-  //       setCategory(value.docs.map((value)=>{
-  //         return value.data() 
-  //       }));
-  //       console.log(category)
-  //     });
-      
-  //   }, [])
-
- 
-    
-
-  // useEffect(()=> {
-  //   const db = getFirestore()
-  //    // const documento = doc (db, "items", prop.name) 
-  //   const data = query(doc(db, "items"), where("category", "==", "novillo"))
-  //   // Ejemplo de aplicacion a proyecto
-  //   // const documento = doc (db, "items", prop.name) 
-  //   getDocs(data)
-
-  //   .then((value)=>{
-  //     setItem(value);
-  //     console.log(item)
-  //   })
-  //   .catch((error)=>console.log(error))
-  // }, [])
-
   useEffect(()=> {
     FetchData(true)
     .then((value)=>{
@@ -97,10 +37,7 @@ function ItemDetailContainer(){
 
     return(
       <div>
-        {/* {category.map((value)=>{
-          // return <nav key={value.id}>{value.title}</nav>
-          return <NavLink to={`/items/${value.category}`}  key={value.id}>{value.id}</NavLink>
-        })} */}
+
         <ItemDetail details={item}/>
       </div>
     )
